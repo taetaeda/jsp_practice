0329d4b03f988e66e980fe28fdfe23a86e1ae084
kang <rainbow2599@naver.com>
2023-09-13 18:19:15 +0900

    많이 배우고2

---
+++ JspBasic/src/main/java/com/jsp/servlet/CookieServlet.java
@@ -0,0 +1,56 @@
+package com.jsp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cookie/login")
+public class CookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CookieServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		if(id.equals("kim1234") && pw.equals("kkk1111!")) {
+			
+			//# 쿠키 생성 방법
+			//1. 쿠키 객체를 생성 - 생성자의 매개값으로 쿠키의 이름과 저장할 데이터 입력(String)
+			Cookie loginCoo = new Cookie("login_cookie", id);
+			
+			//2. 쿠키 클래스의 setter 메서드로 쿠키의 속성을 설정.
+			loginCoo.setMaxAge(5); //쿠키의 유효 시간 설정(초) 1시간 -> 60 * 60
+			
+			//3. http 응답 시 response 객체에 생성된 쿠키를 탑재해서 클라이언트에게 전송.
+			response.addCookie(loginCoo);
+			
+			// 사용자가 아이디 기억하기 체크박스를 체크했는지의 여부 확인.
+			if(request.getParameter("rememberId") != null) { //체크를 했다면 null이 아님.
+				Cookie idMemory = new Cookie("remember_id", id);
+				idMemory.setMaxAge(30);
+				response.addCookie(idMemory);
+			}
+			
+			
+			response.sendRedirect("/JspBasic/cookie/cookie_welcome.jsp");
+			
+		} else {
+			response.sendRedirect("/JspBasic/cookie/cookie_login.jsp");
+		}
+		
+	}
+
+}
---
+++ JspBasic/src/main/webapp/cookie/cookie_login.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form action="/JspBasic/cookie/login" method="post">
+		<input type="text" name="id" size="10" placeholder="ID" value="${cookie.remember_id.value}"> 
+		<input type="checkbox" name="rememberId" value="true"> <small style="font-size: 0.7em">아이디 기억하기</small>
+		<br>
+		<input type="password" name="pw" size="10" placeholder="PW"> <br>
+		<input type="submit" value="로그인">
+	</form>
+
+
+</body>
+</html>
+
+
+
